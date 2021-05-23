20fe1b2ca735c6faad1bc561893ecd4400166413
Alexis Tejeda <alexis.tejeda@gmail.com>
2021-05-23 09:05:50 -0400

    Adding DHT22 smoke test.

---
+++ test/test_dht22.py
@@ -0,0 +1,25 @@
+import unittest
+import logging
+import configparser
+
+import Adafruit_DHT as adafruit
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] - %(message)s'
+)
+
+GPIO_PIN = None
+
+class TestSensor(unittest.TestCase):
+
+    def test_sensor(self):
+        h,t = adafruit.read_retry(adafruit.DHT22, GPIO_PIN)
+        logging.info('h = %s, t = %s', h, t)
+        self.assertTrue(h is not None and t is not None)
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('%s.ini' % __file__.split('.')[0])
+    GPIO_PIN = config['RASPBERRY']['GPIO_DHT22']
+    unittest.main()
